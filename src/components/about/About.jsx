9024319aee2fa3dc221e3cdb6e508e0285adc1b5
Paulina Gonzalez <paulinagzzc@gmail.com>
2023-03-16 13:16:04 -0400

    Added missing photo

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './about.css';
-import AboutImg from '../../assets/memoji.png';
 import CV from '../../assets/CV-PaulinaGonzalez.pdf';
 import Info from './Info';
 
@@ -10,7 +9,7 @@ const About = () => {
       <h2 className="section__title">About Me</h2>
       <span className="section__subtitle">My Introduction</span>
       <div className="about__container container grid">
-        <img src={AboutImg} alt="" className="about__img" />
+        <div className="about__img"></div>
 
         <div className="about__data">
           <Info />
