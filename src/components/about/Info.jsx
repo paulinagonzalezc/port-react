6a09bb923f43ff3365d583fe27786dedef413477
Paulina Gonzalez <paulinagzzc@gmail.com>
2023-04-21 18:09:04 -0600

    Add new rails react project

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -7,7 +7,7 @@ const info = () => {
         <i className="bx bx-award about__icon"></i>
 
         <h3 className="about__title">Experience</h3>
-        <span className="about__subtitle">1 Year developing</span>
+        <span className="about__subtitle">2 Years developing</span>
       </div>
 
       <div className="about__box">
