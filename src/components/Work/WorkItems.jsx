d505df11708f13736cf42634a493f388f3608b86
Paulina Gonzalez <paulinagzzc@gmail.com>
2023-06-27 19:18:44 -0600

    Add modals to projects

--- src/components/Work/WorkItems.jsx
+++ src/components/Work/WorkItems.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const WorkItems = ({item}) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
   return (
     <div className="work__card" key={item.id}>
       <img src={item.image} alt="" className="work__img" />
@@ -11,6 +17,22 @@ const WorkItems = ({item}) => {
       <a href={item.repo} className="work__button">
         Repository <i className="bx bx-right-arrow-alt work__button-icon"></i>
       </a>
+      <button onClick={toggleModal} className="work__button">
+        View Details
+      </button>
+
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal__content">
+            <img src={item.image} alt="" className="modal__img" />
+            <h3 className="modal__title">{item.title}</h3>
+            <p className="modal__description">{item.description}</p>
+            <button onClick={toggleModal} className="modal__close-button">
+              Close
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
